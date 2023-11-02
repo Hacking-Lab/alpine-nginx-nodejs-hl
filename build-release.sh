ab549b55518eed04baf2694962c7cfa94f946cd0
ibuetler <ivan.buetler@hacking-lab.com>
2023-11-02 22:18:18 +0100

    fixing s6

--- build-release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker build --no-cache -t hackinglab/alpine-nginx-nodejs:$1.0 -t hackinglab/alpine-nginx-nodejs:$1 -t hackinglab/alpine-nginx-nodejs:latest -f Dockerfile .
-
-docker push hackinglab/alpine-nginx-nodejs
-docker push hackinglab/alpine-nginx-nodejs:$1
-docker push hackinglab/alpine-nginx-nodejs:$1.0
-
